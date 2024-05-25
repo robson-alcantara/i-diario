52c76c0ed3aac406beba3868c2ae59fa7c172953
Robson Santana <robson.poli@gmail.com>
2024-05-25 13:16:34 -0300

    Ajustes no código

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,6 +44,8 @@ Rails.application.configure do
   # Set to :debug to see everything in the log.
   config.log_level = :info
 
+  config.logger = Logger.new(Rails.root.join ("log/production.log"))
+
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
 
